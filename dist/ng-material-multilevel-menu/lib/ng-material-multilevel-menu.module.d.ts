6a8ddf58027eb155a23327b0cebcd56b951400ea
Ali <ali@Alis-MacBook-Pro.local>
2020-04-21 04:57:26 +0430

    build production library

--- dist/ng-material-multilevel-menu/lib/ng-material-multilevel-menu.module.d.ts
+++ dist/ng-material-multilevel-menu/lib/ng-material-multilevel-menu.module.d.ts
@@ -1,12 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './ng-material-multilevel-menu.component';
-import * as ɵngcc2 from './list-item/list-item.component';
-import * as ɵngcc3 from '@angular/common';
-import * as ɵngcc4 from './materials.module';
-import * as ɵngcc5 from '@angular/router';
 export declare class NgMaterialMultilevelMenuModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<NgMaterialMultilevelMenuModule, [typeof ɵngcc1.NgMaterialMultilevelMenuComponent, typeof ɵngcc2.ListItemComponent], [typeof ɵngcc3.CommonModule, typeof ɵngcc4.MaterialsModule, typeof ɵngcc5.RouterModule], [typeof ɵngcc1.NgMaterialMultilevelMenuComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<NgMaterialMultilevelMenuModule>;
 }
-
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibmctbWF0ZXJpYWwtbXVsdGlsZXZlbC1tZW51Lm1vZHVsZS5kLnRzIiwic291cmNlcyI6WyJuZy1tYXRlcmlhbC1tdWx0aWxldmVsLW1lbnUubW9kdWxlLmQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUE7OztBQUNBIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlY2xhcmUgY2xhc3MgTmdNYXRlcmlhbE11bHRpbGV2ZWxNZW51TW9kdWxlIHtcbn1cbiJdfQ==
