33d7501b3a0a9be4160dd06469e7fdf7a541fdd2
Shashank <shanshank69@gmail.com>
2020-04-24 14:13:58 +0530

    V4.12.0 Build update

--- dist/ng-material-multilevel-menu/lib/materials.module.d.ts
+++ dist/ng-material-multilevel-menu/lib/materials.module.d.ts
@@ -1,10 +1,2 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/material/icon';
-import * as ɵngcc2 from '@angular/material/list';
-import * as ɵngcc3 from '@angular/material/core';
 export declare class MaterialsModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<MaterialsModule, never, [typeof ɵngcc1.MatIconModule, typeof ɵngcc2.MatListModule, typeof ɵngcc3.MatRippleModule], [typeof ɵngcc1.MatIconModule, typeof ɵngcc2.MatListModule, typeof ɵngcc3.MatRippleModule]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<MaterialsModule>;
 }
-
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWF0ZXJpYWxzLm1vZHVsZS5kLnRzIiwic291cmNlcyI6WyJtYXRlcmlhbHMubW9kdWxlLmQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7OztBQUFBOzs7QUFDQSIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWNsYXJlIGNsYXNzIE1hdGVyaWFsc01vZHVsZSB7XHJcbn1cclxuIl19
